356a59fbf8b400b992d2821c377560470d2910c0
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-11-09 23:00:52 +0200

    changing branches

--- src/main/java/com/example/FitFat/Repositories/TrainerRepository.java
+++ src/main/java/com/example/FitFat/Repositories/TrainerRepository.java
@@ -4,6 +4,6 @@ import com.example.FitFat.Models.Trainer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TrainerRepository extends JpaRepository<Trainer, Long> {
-    Trainer findByUsername(String name);
-    Trainer findUserById (Long id);
+    Trainer findByEmail(String email);
+
 }
